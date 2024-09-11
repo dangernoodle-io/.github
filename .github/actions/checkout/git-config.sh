8ddc204c106bae5dcab3a96904ea8fc6cfc3c0df
Jae Gangemi <jgangemi@gmail.com>
2024-09-11 17:39:11 -0600

    - wip

--- .github/actions/checkout/git-config.sh
+++
@@ -1,5 +0,0 @@
-git config user.name 'dangernoodle build bot'
-git config user.email build-bot@dangernoodle.io
-git config commit.gpgsign true
-git config tag.gpgsign true
-git config push.gpgsign if-asked
